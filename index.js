a910ad6e4c4fb6ccb1f5cbeb2c0bf5b28b066441
jesse sheehan <jessetsheehan@gmail.com>
2022-06-14 23:41:09 -0400

    added license stuff, but it may be broken

---
+++ index.js
@@ -0,0 +1,135 @@
+const inquirer = require("inquirer");
+
+const generateReadme = require("./src/readme-template.js");
+
+//
+const {writeFile} = require("./utils/generate-readme.js");
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title for your readme??",
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log("Enter a title for your readme!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter a description for the readme:",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a description");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please enter the installation instructions:",
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log("Enter the installation instructions.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "usageInfo",
+            message: "Please enter the usage information:",
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log("Enter the usage information.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "guidelines",
+            message: "Please enter the the contribution guidelines:",
+            validate: guidelineInput => {
+                if (guidelineInput) {
+                    return true;
+                } else {
+                    console.log("Enter the contribution guidelines.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "testInstructions",
+            message: "Please enter the test instructions:",
+            validate: testInstructionInput => {
+                if (testInstructionInput) {
+                    return true;
+                } else {
+                    console.log("Enter the test instructions.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Please choose a license.",
+            choices: ["Apache License 2.0", "GNU GPLv3", "ISC", "MIT", ]
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter your Github username:",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Enter your Github username.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email address:",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Enter your email address");
+                    return false;
+                }
+            }
+        },
+    ]);
+};
+
+promptUser()
+//THIS IS NOT RIGHT
+// I need to get the info from promptUser into an array
+//or something so it can be put through the generate readme function.
+    .then(promptUserData => {
+        return generateReadme(promptUserData);
+    })
+    .then (pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .catch(err => {
+        console.log(err);
+    });
---
+++ src/readme-template.js
@@ -0,0 +1,72 @@
+const generateLicense = licenseChoice => {
+    switch (licenseChoice) {
+        case "Apache License 2.0":
+            return `
+            [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+            `
+            
+
+        case "GNU GPLv3":
+            return `
+            [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+            `
+            
+        case "ISC":
+            return `
+            [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+
+            `
+            
+        case "MIT":
+            return `
+            [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+            `
+                  
+    }
+}
+
+
+module.exports = templateData => {
+    //destructure projects and about data from templateData based on their property key names
+    //destructure page data by section
+    //the ... in this situation is a rest/spread operator... not sure which one.
+    const { title, description, installation, usageInfo, guidelines, testInstructions, license, email, github } = templateData;
+
+    return `
+
+# ${title}
+
+## Table of Contents
+* [Overview](#Overview)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Guidelines](#Guidelines)
+* [Test Instructions](#TestInstructions)
+* [License](#License) 
+* [Questions](#Questions)
+
+## Overview
+${description}
+
+## Installation
+${installation}
+
+## Usage
+${usageInfo}
+
+## Guidelines
+${guidelines}
+
+## Test Instructions
+${testInstructions}
+
+## License
+${generateLicense(license)}
+
+## Questions
+For additional questions, please contact [${github}](https://github.com/${github}) at ${email}
+
+
+    `
+};
